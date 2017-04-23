7fa621b5c8ef756234ce24880b67bb82cbbe9232
Benjamin Scabbia <Benjamin Scabbia>
2017-04-23 14:49:34 +0000

    fixed typo

--- x-ray-debug.js
+++ x-ray-debug.js
@@ -3,9 +3,9 @@ javascript: (
         var style = "<style>*{background:#000!important;color:#0f0!important;outline:solid #f00 1px!important;}</style>";
         var elements = document.body.getElementsByTagName("*");
         var items = [];
-        for (var i = 0; i < element.length; i++) {
+        for (var i = 0; i < elements.length; i++) {
             if (elements[i].innerHTML.indexOf("background:#000!important;color:#0f0!important;outline:solid #f00 1px!important;") != -1) {
-                items.push(element[i]);
+                items.push(elements[i]);
             }
         }
 
