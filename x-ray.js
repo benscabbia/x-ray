3ef5a7251f82f00d62e2733cb819147e77b36446
Samuel Degueldre <42469486+sdegueldre@users.noreply.github.com>
2019-07-31 13:28:53 +0200

    Update x-ray.js

--- x-ray.js
+++ x-ray.js
@@ -1,19 +1,12 @@
 javascript: (
-    function () {
-        var elements = document.body.getElementsByTagName("*");
-        var items = [];
-        for (var i = 0; i < elements.length; i++) {
-            if (elements[i].innerHTML.indexOf("background:#000!important;color:#0f0!important;outline:solid #f00 1px!important;") != -1) {
-                items.push(elements[i]);
-            }
-        }
-
-        if (items.length > 0) {
-            for (var i = 0; i < items.length; i++) {
-                document.body.removeChild(items[i]);
-            }
-        } else {
-            document.body.innerHTML += "<style>*{background:#000!important;color:#0f0!important;outline:solid #f00 1px!important;}</style>";
-        }
+  function () {
+    const xray = document.createElement('style');
+    xray.innerHTML = "*{background:#000!important;color:#0f0!important;outline:solid #f00 1px!important;}";
+    const xraysInPage = [...document.body.getElementsByTagName("style")].filter(style => style.innerHTML === xray.innerHTML);
+    if(xraysInPage.length > 0) {
+      xraysInPage.forEach(style => document.body.removeChild(style));
+    } else {
+      document.body.appendChild(xray)
     }
-)();    
+  }
+)();
